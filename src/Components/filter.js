350238bc5c6eff9c2786d18d33d9cdf642684b08
mke2111 <roymke134@gmail.com>
2021-04-13 00:07:51 +0300

    Add filter component

---
+++ src/Components/filter.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const categories = ['Pizza', 'Pasta', 'Spaghetti', 'Couscous', 'Noodles', 'Sauce', 'Candy'];
+
+const getCategories = () => {
+  const result = [];
+  for (let i = 0; i < categories.length; i += 1) {
+    result.push(
+      <option key={categories[i]} value={categories[i]}>
+        {categories[i]}
+      </option>,
+    );
+  }
+  return result;
+};
+
+const Filter = ({ updateFilter, filter }) => {
+  const handleChange = (e) => {
+    updateFilter(e.target.value);
+  };
+  return (
+    <div id="search">
+      <select onChange={handleChange} data-testid="categories" name="categories" id="categories" defaultValue={filter}>
+        {getCategories()}
+      </select>
+    </div>
+  );
+};
+
+Filter.propTypes = {
+  updateFilter: PropTypes.func.isRequired,
+  filter: PropTypes.string.isRequired,
+};
+
+export default Filter;
