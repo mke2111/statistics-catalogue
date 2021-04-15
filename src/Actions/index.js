2677206df052f08f9a5a6233715a0be7c0febf86
mke2111 <roymke134@gmail.com>
2021-04-15 19:49:20 +0300

    Add filter action

--- src/Actions/index.js
+++ src/Actions/index.js
@@ -3,6 +3,9 @@ import axios from 'axios';
 export const STOCK_REQUEST = 'STOCK_REQUEST';
 export const STOCK_SUCCESS = 'STOCK_SUCCESS';
 export const STOCK_FAILURE = 'STOCK_FAILURE';
+export const CHANGE_FILTER = 'CHANGE_FILTER';
+
+export const changeFilter = (filter) => ({ type: CHANGE_FILTER, filter });
 
 const url = 'https://financialmodelingprep.com/api/v3/quotes/crypto?apikey=c9576c43313a8d36d1d6049b18a12180';
 
