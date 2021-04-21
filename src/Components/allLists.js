9c32e84e7a9d50aff86e0e46f53fbf09afbd92fb
mke2111 <roymke134@gmail.com>
2021-04-21 18:40:07 +0300

    Update filter

--- src/Components/allLists.js
+++ src/Components/allLists.js
@@ -3,19 +3,23 @@ import PropTypes from 'prop-types';
 
 function StockCard({ stock }) {
   return (
-    <div className="size main">
+    <div className="size main" key={stock.id}>
       <p>{stock.ticker}</p>
-      <a href={`\\stock\\${stock.ticker}`} className=" w-100"><p>{stock.companyName}</p></a>
+      <p>{stock.symbol}</p>
+      <a href={`\\stock\\${stock.ticker}`} className=" w-100"><p>{stock.name}</p></a>
       <p>{stock.price}</p>
       <p>{stock.currency}</p>
+      <p>{stock.changesPercentage}</p>
     </div>
   );
 }
 
 StockCard.propTypes = {
   stock: PropTypes.shape({
-    ticker: PropTypes.string.isRequired,
-    companyName: PropTypes.string.isRequired,
+    ticker: PropTypes.string,
+    symbol: PropTypes.string.isRequired,
+    id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
     price: PropTypes.string.isRequired,
     currency: PropTypes.string,
     changesPercentage: PropTypes.string.isRequired,
--- src/Components/navbar.js
+++ src/Components/navbar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Form, Button } from 'react-bootstrap';
+import { Form } from 'react-bootstrap';
 
 const Navbar = ({ handleOnSelect }) => (
   <>
@@ -8,11 +8,11 @@ const Navbar = ({ handleOnSelect }) => (
       <h3 className="text-center col m-0 p-0"><a href="./">biit</a></h3>
       <Form inline>
         <Form.Control onChange={handleOnSelect} as="select" className="mr-sm-2">
-          <option value="active">Most Active </option>
-          <option value="gainers">Most Gainers </option>
-          <option value="losers">Most Losers </option>
+          <option value="crypto">CRYPTO </option>
+          <option value="index">INDEX </option>
+          <option value="forex">FOREX </option>
         </Form.Control>
-        <Button variant="outline-success">Filter by category</Button>
+        {/* <Button variant="outline-success">Filter by category</Button> */}
       </Form>
     </nav>
   </>
